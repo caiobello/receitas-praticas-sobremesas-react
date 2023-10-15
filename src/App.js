d8a70d9b9866fb38f5cac45497cf93035e615679
Caio Zamana <caiobzm@gmail.com>
2023-10-15 16:49:43 -0300

    css

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
+import { Footer } from './components/Footer';
 import ReceitasPraticasSobremesas from './components/ReceitasPraticasSobremesas';
 
 function App() {
   return (
     <div className="App">
       <ReceitasPraticasSobremesas />
+      <Footer />
 
 
     </div>
